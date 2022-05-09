2388897110b032f2f4f65ea0c28428aab9743ed1
Dean Howe <deanhowe@gmail.com>
2022-05-09 14:43:43 +0100

    0.1.44
    commited for the sake of commting… should probably clean up this repo and start again, but that’s unlikely to happen.

--- Makefile
+++ Makefile
@@ -1,30 +1,39 @@
 ASSETS := $(shell yq e '.assets.[].src' manifest.yaml)
 ASSET_PATHS := $(addprefix assets/,$(ASSETS))
 VERSION := $(shell yq e ".version" manifest.yaml)
-LNBITS_LITE_SRC := $(shell find ./lnbits-lite/src) lnbits-lite/Cargo.toml lnbits-lite/Cargo.lock
+LNBITS_LITE_SRC := $(shell find ./lnbits-legend/lnbits)
+#MANAGER_SRC := $(shell find ./manager -name '*.rs') manager/Cargo.toml manager/Cargo.lock
+
 S9PK_PATH=$(shell find . -name lnbits-lite.s9pk -print)
 
+# delete the target of a rule if it has changed and its recipe exits with a nonzero exit status
 .DELETE_ON_ERROR:
 
 all: verify
 
 verify: lnbits-lite.s9pk $(S9PK_PATH)
-		embassy-sdk verify $(S9PK_PATH)
+		embassy-sdk verify s9pk $(S9PK_PATH)
 
-#install: lnbits.s9pk
-#	embassy-cli package install lnbits.s9pk
-
-lnbits-lite.s9pk: manifest.yaml config_rules.yaml image.tar docs/instructions.md icon.png $(ASSET_PATHS)
+clean:
+	rm -f image.tar
+	rm -f lnbits-lite.s9pk
+	
+lnbits-lite.s9pk: config_spec.yaml config_rules.yaml image.tar instructions.md $(ASSET_PATHS)
 	embassy-sdk pack
 		
 # --security-opt=seccomp=unconfined --tag
 
-image.tar: Dockerfile docker_entrypoint.sh lnbits-lite/target/aarch64-unknown-linux-musl/release/lnbits-lite
+image.tar: Dockerfile docker_entrypoint.sh check-web.sh configs-get.sh $(LNBITS_LITE_SRC)
 		DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build --tag start9/lnbits-lite/main:$(VERSION) --platform=linux/arm64 -o type=docker,dest=image.tar .
-		
-lnbits-lite/target/aarch64-unknown-linux-musl/release/lnbits-lite: $(LNBITS_LITE_SRC)
+
+#manager/target/aarch64-unknown-linux-musl/release/bitcoind-manager: $(MANAGER_SRC)
+#	docker run --rm -it -v ~/.cargo/registry:/root/.cargo/registry -v "$(shell pwd)"/manager:/home/rust/src start9/rust-musl-cross:aarch64-musl cargo build --release
+	
+#lnbits-lite/target/aarch64-unknown-linux-musl/release/lnbits-lite
+#lnbits-lite/target/aarch64-unknown-linux-musl/release/lnbits-lite: $(LNBITS_LITE_SRC)
+#		docker run --rm -it -v ~/.cargo/registry:/root/.cargo/registry -v "$(shell pwd)"/lnbits-lite:/home/rust/src start9/rust-musl-cross:aarch64-musl cargo build --release
 		#docker run --rm -it -v ~/.cargo/registry:/root/.cargo/registry -v "$(shell pwd)"/lnbits-lite:/home/rust/src start9/rust-musl-cross:aarch64-musl cargo +beta build --release
-		docker run --rm -it -v ~/.cargo/registry:/root/.cargo/registry -v "$(shell pwd)"/lnbits-lite:/home/rust/src start9/rust-musl-cross:aarch64-musl musl-strip target/aarch64-unknown-linux-musl/release/lnbits-lite
+		
 
 #manifest.yaml: lnbits-lite/Cargo.toml
 #		yq e -i '.version="$(VERSION)"' manifest.yaml
--- scripts/echo_lnbits_envs.sh
+++ scripts/echo_lnbits_envs.sh
@@ -1,88 +1,46 @@
 #!/bin/sh
 
 # Load the env/config values from EmbassyOS
-##. /home/lnbits/.local/set_lnbits_envs
 
-#for i in $(seq 0 $(($NUM_ACCOUNTS-1)))
-#for i in $(seq 0 $(($NUM_ACCOUNTS-1)))
-
-# Print out all the env/config values from Embassy OS
-##echo "EMBASSY_LNBITS_BACKEND_WALLET_CLASS = $EMBASSY_LNBITS_BACKEND_WALLET_CLASS"
-##echo "EMBASSY_QUART_APP = $EMBASSY_QUART_APP"
-##echo "EMBASSY_QUART_ENV = $EMBASSY_QUART_ENV"
-##echo "EMBASSY_QUART_DEBUG = $EMBASSY_QUART_DEBUG"
-##
-##echo "Force https : $EMBASSY_LNBITS_FORCE_HTTPS"
-##echo "Disabled Extentions : $EMBASSY_LNBITS_DISABLED_EXTENSIONS"
-##
-##echo "EMBASSY_LNBITS_ALLOWED_USERS = $EMBASSY_LNBITS_ALLOWED_USERS"
-##
-##echo "EMBASSY_LNBITS_SITE_TITLE = $EMBASSY_LNBITS_SITE_TITLE"
-##echo "EMBASSY_LNBITS_SITE_TAGLINE = $EMBASSY_LNBITS_SITE_TAGLINE"
-##echo "EMBASSY_LNBITS_SITE_DESCRIPTION = $EMBASSY_LNBITS_SITE_DESCRIPTION"
-##
-##echo "EMBASSY_LNBITS_DEFAULT_WALLET_NAME = $EMBASSY_LNBITS_DEFAULT_WALLET_NAME"
-##echo "EMBASSY_LNBITS_THEME_OPTIONS = $EMBASSY_LNBITS_THEME_OPTIONS"
-##
-##echo "Service Fee :$EMBASSY_LNBITS_SERVICE_FEE"
-##
-##echo "EMBASSY_LNBITS_LNPAY_API_ENDPOINT = $EMBASSY_LNBITS_LNPAY_API_ENDPOINT"
-##echo "EMBASSY_LNBITS_LNPAY_API_KEY = $EMBASSY_LNBITS_LNPAY_API_KEY"
-##echo "EMBASSY_LNBITS_LNPAY_ADMIN_KEY = $EMBASSY_LNBITS_LNPAY_ADMIN_KEY"
-##
-##echo "EMBASSY_LNBITS_LND_HOST = $EMBASSY_LNBITS_LND_HOST"
-##
-##echo "EMBASSY_LND_REST_MACAROON = $EMBASSY_LND_REST_MACAROON"
-##echo "EMBASSY_LND_REST_MACAROON = $EMBASSY_LND_REST_MACAROON"
-##
-##echo "EMBASSY_SPARK_URL = $EMBASSY_SPARK_URL"
-##
-##echo "EMBASSY_ACCOUNT_SPARK_URL = $EMBASSY_ACCOUNT_SPARK_URL"
-##echo "EMBASSY_ACCOUNT_SPARK_URL = $EMBASSY_ACCOUNT_SPARK_URL"
-##echo "EMBASSY_SPARK_TOKEN = $EMBASSY_SPARK_TOKEN"
-##
-##echo "EMBASSY_LNBITS_API_ENDPOINT = $EMBASSY_LNBITS_API_ENDPOINT"
-##echo "EMBASSY_LNBITS_ADMIN_KEY = $EMBASSY_LNBITS_ADMIN_KEY"
-
-NUM_ACCOUNTS=$(yq e ".instances | length" /relay/start9/config.yaml)
+NUM_ACCOUNTS=$(yq e ".instances | length" /datadir/start9/config.yaml)
 
 for i in $(seq 0 $(($NUM_ACCOUNTS-1)))
 do
 
-TYPE=$(yq e ".instances[$i].connection-settings.type" /relay/start9/config.yaml)
+TYPE=$(yq e ".instances[$i].connection-settings.type" /datadir/start9/config.yaml)
 
-LNBITS_ALLOWED_USERS=$(yq e ".instances[$i].lnbits-allowed-users" /relay/start9/config.yaml )
+LNBITS_ALLOWED_USERS=$(yq e ".instances[$i].lnbits-allowed-users" /datadir/start9/config.yaml )
 if [ "$LNBITS_ALLOWED_USERS" = "~" ]; then
 	LNBITS_ALLOWED_USERS=""
 fi
 
-LND_REST_ENDPOINT=$(yq e ".instances[$i].connection-settings.addressext" /relay/start9/config.yaml ):$(yq e ".instances[$i].connection-settings.port" /relay/start9/config.yaml )
-LND_REST_CERT=$(yq e ".instances[$i].connection-settings.certificate" /relay/start9/config.yaml )
-LND_REST_MACAROON=$(yq e ".instances[$i].connection-settings.macaroon" /relay/start9/config.yaml )
+LND_REST_ENDPOINT_INPUT=$(yq e ".instances[$i].connection-settings.addressext" /datadir/start9/config.yaml ):$(yq e ".instances[$i].connection-settings.port" /datadir/start9/config.yaml )
+LND_REST_CER_INPUTT=$(yq e ".instances[$i].connection-settings.certificate" /datadir/start9/config.yaml )
+LND_REST_MACAROON_INPUT=$(yq e ".instances[$i].connection-settings.macaroon" /datadir/start9/config.yaml )
 
 echo "
-QUART_APP=$(yq e ".instances[$i].lnbits-quart.lnbits-quart-app" /relay/start9/config.yaml )
-QUART_ENV=$(yq e ".instances[$i].lnbits-quart.lnbits-quart-env" /relay/start9/config.yaml )
-QUART_DEBUG=$(yq e ".instances[$i].lnbits-quart.lnbits-quart-debug" /relay/start9/config.yaml )
+QUART_APP=$(yq e ".instances[$i].lnbits-quart.lnbits-quart-app" /datadir/start9/config.yaml )
+QUART_ENV=$(yq e ".instances[$i].lnbits-quart.lnbits-quart-env" /datadir/start9/config.yaml )
+QUART_DEBUG=$(yq e ".instances[$i].lnbits-quart.lnbits-quart-debug" /datadir/start9/config.yaml )
 
 HOST=0.0.0.0
 PORT=500$i
 
 LNBITS_ALLOWED_USERS=\"$LNBITS_ALLOWED_USERS\"
-LNBITS_DEFAULT_WALLET_NAME=\"$(yq e ".instances[$i].lnbits-site-meta.default-wallet-name" /relay/start9/config.yaml )\"
+LNBITS_DEFAULT_WALLET_NAME=\"$(yq e ".instances[$i].lnbits-site-meta.default-wallet-name" /datadir/start9/config.yaml )\"
 
-LNBITS_DATA_FOLDER=\"/relay/lnbits/lnbits-legend/500$i/data\"
+LNBITS_DATA_FOLDER=\"/datadir/lnbits/lnbits-legend/500$i/data\"
 
 # LNBITS_DATABASE_URL="postgres://user:password@host:port/databasename"
 
-LNBITS_DISABLED_EXTENSIONS=\"$(yq e ".instances[$i].lnbits-quart.lnbits-disabled-extentions" /relay/start9/config.yaml )\"
-LNBITS_FORCE_HTTPS=$(yq e ".instances[$i].lnbits-quart.lnbits-force-https" /relay/start9/config.yaml )
-LNBITS_SERVICE_FEE=\"$(yq e ".instances[$i].lnbits-service-fee" /relay/start9/config.yaml )\"
+LNBITS_DISABLED_EXTENSIONS=\"$(yq e ".instances[$i].lnbits-quart.lnbits-disabled-extentions" /datadir/start9/config.yaml )\"
+LNBITS_FORCE_HTTPS=$(yq e ".instances[$i].lnbits-quart.lnbits-force-https" /datadir/start9/config.yaml )
+LNBITS_SERVICE_FEE=\"$(yq e ".instances[$i].lnbits-service-fee" /datadir/start9/config.yaml )\"
 
-LNBITS_SITE_TITLE=\"$(yq e ".instances[$i].lnbits-site-meta.lnbits-site-title" /relay/start9/config.yaml )\"
-LNBITS_SITE_TAGLINE=\"$(yq e ".instances[$i].lnbits-site-meta.lnbits-site-tagline" /relay/start9/config.yaml )\"
-LNBITS_SITE_DESCRIPTION=\"$(yq e ".instances[$i].lnbits-site-meta.lnbits-site-description" /relay/start9/config.yaml )\"
-LNBITS_THEME_OPTIONS=\"$(yq e ".instances[$i].lnbits-site-meta.lnbits-theme-option" /relay/start9/config.yaml )\"
+LNBITS_SITE_TITLE=\"$(yq e ".instances[$i].lnbits-site-meta.lnbits-site-title" /datadir/start9/config.yaml )\"
+LNBITS_SITE_TAGLINE=\"$(yq e ".instances[$i].lnbits-site-meta.lnbits-site-tagline" /datadir/start9/config.yaml )\"
+LNBITS_SITE_DESCRIPTION=\"$(yq e ".instances[$i].lnbits-site-meta.lnbits-site-description" /datadir/start9/config.yaml )\"
+LNBITS_THEME_OPTIONS=\"$(yq e ".instances[$i].lnbits-site-meta.lnbits-theme-option" /datadir/start9/config.yaml )\"
 " > /home/lnbits/app/500$i/.env
 
 if [[ "$TYPE" == "CLightningWallet" ]]
@@ -90,7 +48,7 @@ then
 	echo "
 LNBITS_BACKEND_WALLET_CLASS=CLightningWallet
 
-CLIGHTNING_RPC=\"/relay/start9/shared/c-lightning/lightning-rpc\"	
+CLIGHTNING_RPC=\"/mnt/c-lightning/lightning-rpc\"	
 " >> /home/lnbits/app/500$i/.env
 
 elif [[ "$TYPE" == "LndWalletInternal" ]]
@@ -98,10 +56,10 @@ then
 	echo "
 LNBITS_BACKEND_WALLET_CLASS=LndWallet
 	
-LND_GRPC_ENDPOINT=$(yq e ".instances[$i].connection-settings.address" /relay/start9/config.yaml )
+LND_GRPC_ENDPOINT=$(yq e ".instances[$i].connection-settings.address" /datadir/start9/config.yaml )
 LND_GRPC_PORT=11009
-LND_GRPC_CERT=\"/relay/start9/public/lnd/tls.cert\"
-LND_GRPC_MACAROON=\"$(xxd -p -c 1000 /home/lnbits/app/admin.macaroon | xargs )\"
+LND_GRPC_CERT=\"/mnt/lnd/tls.cert\"
+LND_GRPC_MACAROON=\"/home/lnbits/app/admin.macaroon\"
 " >> /home/lnbits/app/500$i/.env
 
 elif [[ "$TYPE" == "LndWallet" ]]
@@ -109,10 +67,10 @@ then
 	echo "
 LNBITS_BACKEND_WALLET_CLASS=LndWallet
 
-LND_GRPC_ENDPOINT=$(yq e ".instances[$i].connection-settings.addressext" /relay/start9/config.yaml )
-LND_GRPC_PORT=$(yq e ".instances[$i].connection-settings.port" /relay/start9/config.yaml )
-LND_GRPC_CERT=\"$(yq e ".instances[$i].connection-settings.certificate" /relay/start9/config.yaml )\"
-LND_GRPC_MACAROON=\"$(yq e ".instances[$i].connection-settings.macaroon" /relay/start9/config.yaml )\"
+LND_GRPC_ENDPOINT=$(yq e ".instances[$i].connection-settings.addressext" /datadir/start9/config.yaml )
+LND_GRPC_PORT=$(yq e ".instances[$i].connection-settings.port" /datadir/start9/config.yaml )
+LND_GRPC_CERT=\"$(yq e ".instances[$i].connection-settings.certificate" /datadir/start9/config.yaml )\"
+LND_GRPC_MACAROON=\"$(yq e ".instances[$i].connection-settings.macaroon" /datadir/start9/config.yaml )\"
 " >> /home/lnbits/app/500$i/.env
 
 elif [[ "$TYPE" == "LndRestWalletInternal" ]]
@@ -120,19 +78,22 @@ then
 	echo "
 LNBITS_BACKEND_WALLET_CLASS=LndRestWallet
 
-LND_REST_ENDPOINT=\"https://$(yq e ".instances[$i].connection-settings.address" /relay/start9/config.yaml ):8080\"
-LND_REST_CERT=\"/relay/start9/public/lnd/tls.cert\"
+LND_REST_ENDPOINT=\"https://$(yq e ".instances[$i].connection-settings.address" /datadir/start9/config.yaml ):8080\"
+LND_REST_CERT=\"/mnt/lnd/tls.cert\"
 LND_REST_MACAROON=\"$(xxd -p -c 1000 /home/lnbits/app/admin.macaroon | xargs)\"
 " >> /home/lnbits/app/500$i/.env
 
 elif [[ "$TYPE" == "LndRestWallet" ]]
 then
+	echo $LND_REST_MACAROON_INPUT >> /home/lnbits/app/500$i/admin.macaroon
+	echo $LND_REST_CERT_INPUT >> /home/lnbits/app/500$i/cert
+	# $(xxd -p -c 1000 /home/lnbits/app/500$i/admin.macaroon | xargs)
 	echo "
 LNBITS_BACKEND_WALLET_CLASS=LndRestWallet
 
 LND_REST_ENDPOINT=\"https://$LND_REST_ENDPOINT\"
-LND_REST_CERT=\"$LND_REST_CERT\"
-LND_REST_MACAROON=\"$LND_REST_MACAROON\"
+LND_REST_CERT=\"/home/lnbits/app/500$i/admin.macaroon\"
+LND_REST_MACAROON=\"/home/lnbits/app/500$i/cert\"
 " >> /home/lnbits/app/500$i/.env
 
 elif [[ "$TYPE" == "SparkWallet" ]]
@@ -140,8 +101,8 @@ then
 	echo "
 LNBITS_BACKEND_WALLET_CLASS=SparkWallet
 
-SPARK_URL=$(yq e ".instances[$i].connection-settings.lnbits-spark-url-with-port" /relay/start9/config.yaml )
-SPARK_TOKEN=$(yq e ".instances[$i].connection-settings.lnbits-spark-token" /relay/start9/config.yaml )
+SPARK_URL=$(yq e ".instances[$i].connection-settings.lnbits-spark-url-with-port" /datadir/start9/config.yaml )
+SPARK_TOKEN=$(yq e ".instances[$i].connection-settings.lnbits-spark-token" /datadir/start9/config.yaml )
 " >> /home/lnbits/app/500$i/.env
 
 elif [[ "$TYPE" == "LNbitsWallet" ]]
@@ -149,8 +110,8 @@ then
 	echo "
 LNBITS_BACKEND_WALLET_CLASS=LNbitsWallet
 
-LNBITS_ENDPOINT=$(yq e ".instances[$i].connection-settings.lnbits-lnbits-api-endpoint" /relay/start9/config.yaml )
-LNBITS_KEY=$(yq e ".instances[$i].connection-settings.lnbits-lnbits-api-admin-key" /relay/start9/config.yaml )
+LNBITS_ENDPOINT=$(yq e ".instances[$i].connection-settings.lnbits-lnbits-api-endpoint" /datadir/start9/config.yaml )
+LNBITS_KEY=$(yq e ".instances[$i].connection-settings.lnbits-lnbits-api-admin-key" /datadir/start9/config.yaml )
 " >> /home/lnbits/app/500$i/.env
 
 elif [[ "$TYPE" == "LNPayWallet" ]]
@@ -158,9 +119,9 @@ then
 	echo "
 LNBITS_BACKEND_WALLET_CLASS=LNPayWallet
 
-LNPAY_API_ENDPOINT=$(yq e ".instances[$i].connection-settings.lnbits-lnpay-api-endpoint" /relay/start9/config.yaml )
-LNPAY_API_KEY=$(yq e ".instances[$i].connection-settings.lnbits-lnpay-api-key" /relay/start9/config.yaml )
-LNPAY_WALLET_KEY=$(yq e ".instances[$i].connection-settings.lnbits-lnpay-wallet-key" /relay/start9/config.yaml )
+LNPAY_API_ENDPOINT=$(yq e ".instances[$i].connection-settings.lnbits-lnpay-api-endpoint" /datadir/start9/config.yaml )
+LNPAY_API_KEY=$(yq e ".instances[$i].connection-settings.lnbits-lnpay-api-key" /datadir/start9/config.yaml )
+LNPAY_WALLET_KEY=$(yq e ".instances[$i].connection-settings.lnbits-lnpay-wallet-key" /datadir/start9/config.yaml )
 " >> /home/lnbits/app/500$i/.env
 
 elif [[ "$TYPE" == "LntxbotWallet" ]]
@@ -168,8 +129,8 @@ then
 	echo "
 LNBITS_BACKEND_WALLET_CLASS=LntxbotWallet
 
-LNTXBOT_API_ENDPOINT=$(yq e ".instances[$i].connection-settings.lnbits-lntxbot-api-endpoint" /relay/start9/config.yaml )
-LNTXBOT_KEY=$(yq e ".instances[$i].connection-settings.lnbits-lntxbot-api-key" /relay/start9/config.yaml )
+LNTXBOT_API_ENDPOINT=$(yq e ".instances[$i].connection-settings.lnbits-lntxbot-api-endpoint" /datadir/start9/config.yaml )
+LNTXBOT_KEY=$(yq e ".instances[$i].connection-settings.lnbits-lntxbot-api-key" /datadir/start9/config.yaml )
 " >> /home/lnbits/app/500$i/.env
 
 elif [[ "$TYPE" == "OpenNodeWallet" ]]
@@ -177,8 +138,8 @@ then
 	echo "
 LNBITS_BACKEND_WALLET_CLASS=OpenNodeWallet
 	
-OPENNODE_API_ENDPOINT=$(yq e ".instances[$1].connection-settings.lnbits-opennode-api-endpoint" /relay/start9/config.yaml )
-OPENNODE_KEY=$(yq e ".instances[$1].connection-settings.lnbits-opennode-api-key" /relay/start9/config.yaml )
+OPENNODE_API_ENDPOINT=$(yq e ".instances[$1].connection-settings.lnbits-opennode-api-endpoint" /datadir/start9/config.yaml )
+OPENNODE_KEY=$(yq e ".instances[$1].connection-settings.lnbits-opennode-api-key" /datadir/start9/config.yaml )
 " >> /home/lnbits/app/500$i/.env
 fi
 done
